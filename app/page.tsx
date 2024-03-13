e3044cd2db572c41045fe3102c89721dc6d02a8e
timnirmal <timnirmal@gmail.com>
2024-03-13 11:28:42 +0530

    basic updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import {useState} from 'react';
-import {ModeToggle} from "@/components/theme_button";
+// import {ModeToggle} from "@/components/theme_button";
 
 export default function Home() {
     const [isHovered, setIsHovered] = useState(false);
@@ -12,7 +12,7 @@ export default function Home() {
             <div className="flex flex-col items-center pt-32">
                 <p className="pt-5 text-4xl pb-16">Machine Learning Enthusiast | Computer Engineering UG @ USJ |
                     Freelancer</p>
-                <h1 className="text-6xl font-bold">Hi, I'm Thimira Nirmal</h1>
+                <h1 className="text-6xl font-bold">Hi, I&apos;m Thimira Nirmal</h1>
                 <p className="text-lg">@timnirmal</p>
                 <div
                     onMouseEnter={() => setIsHovered(true)}
